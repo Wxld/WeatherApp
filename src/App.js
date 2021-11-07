ce9306c9fc47b39072a1bb0e360b269ea21f92a8
Ranjeet <ranjeetb623@gmail.com>
2021-11-07 23:28:30 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Home from './Components/Home'
 import Weather from './Components/Weather'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path={process.env.PUBLIC_URL + '/'} exact component={Home} />
+          <Route path={'/'} exact component={Home} />
           <Route path="/weather" component={Weather} />
         </Switch>
       </div>
